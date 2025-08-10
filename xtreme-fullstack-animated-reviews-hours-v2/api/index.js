1bee077baa53869586ae83424c8df2162ab340cc
jithin99 <141995631+jithin99@users.noreply.github.com>
2025-08-10 17:05:09 +0530

    Update index.js

--- xtreme-fullstack-animated-reviews-hours-v2/api/index.js
+++ xtreme-fullstack-animated-reviews-hours-v2/api/index.js
@@ -2,8 +2,8 @@
 import express from 'express'
 import cors from 'cors'
 import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc.js'
+import timezone from 'dayjs/plugin/timezone.js'
 dayjs.extend(utc); dayjs.extend(timezone)
 
 import jwt from 'jsonwebtoken'
