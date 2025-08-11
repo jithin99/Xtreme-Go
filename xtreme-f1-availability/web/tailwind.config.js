5652f5396f2918f1fa3afd787f845c1314fff15d
jithin99 <141995631+jithin99@users.noreply.github.com>
2025-08-11 22:02:50 +0530

    Update tailwind.config.js

--- xtreme-f1-availability/web/tailwind.config.js
+++ xtreme-f1-availability/web/tailwind.config.js
@@ -1,6 +1,9 @@
+// web/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html','./src/**/*.{jsx,js}'],
-  theme: { extend: {} },
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
