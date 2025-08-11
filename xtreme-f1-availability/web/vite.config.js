efc2510c42d24c8d2c634ffb71850ab997369ac5
jithin99 <141995631+jithin99@users.noreply.github.com>
2025-08-11 22:04:27 +0530

    Update vite.config.js

--- xtreme-f1-availability/web/vite.config.js
+++ xtreme-f1-availability/web/vite.config.js
@@ -1,7 +1,4 @@
+// web/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  server: { port:5173, host:true }
-})
+export default defineConfig({ plugins: [react()], server:{ port:5173, host:true } })
